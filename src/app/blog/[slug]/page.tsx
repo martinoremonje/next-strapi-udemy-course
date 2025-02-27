34f581188688d20ac14716421a182c3963591326
martinoremonje <m.monjeorellana@gmail.com>
2025-02-27 11:37:36 -0300

    cambios de page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,56 +1,50 @@
 import { fetchApi } from '@/helpers/fetch-api';
 import { Post } from '@/interfaces/post';
 import { notFound } from 'next/navigation';
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 import PageHeader from '@/components/PageHeader';
 import { formatDate } from '@/helpers/format-date-helper';
 
-
-
-const getData = async(slug = "") =>{
+const getData = async(slug = ""): Promise<Post | null> => {
     const path = "/posts";
     const urlParamsObject = {
-    populate: "*",
-   filters: {
-    slug
-   }
-};
-const {data} = await fetchApi(path, urlParamsObject);
-return data[0]
+        populate: "*",
+        filters: { slug }
+    };
+    const { data } = await fetchApi(path, urlParamsObject);
+    return data[0] || null;
 };
 
 interface Props {
     params: {
-        slug: string
-    }
+        slug: string;
+    };
 }
 
-const Slug = async({params}: Props) => {
-
-    const post: Post = await getData(params.slug)
+const Slug = async({ params }: Props) => {
+    const post: Post | null = await getData(params.slug);
 
     if(!post) {
-       return notFound()
+       return notFound();
     }
 
-    const {title, body, createdAt, image} = post;
-    const {url, width, height} = image.formats.medium
-    const bodie = body[0].children[0].text
-  return (
-    <div className='space-y-8'>
-        <PageHeader title={title} />
-        <p className='text-gray-500'>
-            {
-                formatDate(createdAt)
-            }
-        </p>
-        <Image className="rounded-t-lg" src={url} alt={`image ${title}`} width={width} height={height} />
-        <div className='prose'>
-        {bodie}
+    const { title, body, createdAt, image } = post;
+    const { url, width, height } = image.formats.medium;
+    const bodie = body[0].children[0].text;
+    
+    return (
+        <div className='space-y-8'>
+            <PageHeader title={title} />
+            <p className='text-gray-500'>
+                {formatDate(createdAt)}
+            </p>
+            <Image className="rounded-t-lg" src={url} alt={`image ${title}`} width={width} height={height} />
+            <div className='prose'>
+                {bodie}
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Slug
+export default Slug;
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,3 @@
-
 import { fetchApi } from '@/helpers/fetch-api'
 import React from 'react'
 import PageHeader from '@/components/PageHeader';
@@ -6,59 +5,49 @@ import PageCardImage from '@/components/PageCardImage';
 import { Post } from '@/interfaces/post';
 import PagePagination from '@/components/PagePagination';
 
-
-
-const getData = async(page = 1, pageSize= 4)=>{
+const getData = async(page = 1, pageSize = 4) => {
   const path = "/posts";
   const urlParamsObject = {
     populate: "*",
-    sort: {
-      createdAt: "asc"
-    },
-    pagination: {
-      page,
-      pageSize
-    }
-  }
-    
-   
-  const {data, meta} = await fetchApi(path, urlParamsObject);
-  return {
-    data, 
-    pagination: meta.pagination
-  }
+    sort: { createdAt: "asc" },
+    pagination: { page, pageSize }
+  };
+  
+  const { data, meta } = await fetchApi(path, urlParamsObject);
+  return { data, pagination: meta.pagination };
 }
 
-interface Props{
-  searchParams:{
-    page?: string
-  }
+interface Props {
+  params: {
+    slug: string;
+  };
+  searchParams: {
+    page?: string;
+  };
 }
 
-const Blog = async ({searchParams}: Props) => {
-    const {page} = searchParams
-    let pageNumber = page ? parseInt(page) : 1;
+const Blog = async ({ params, searchParams }: Props) => {
+  const { page } = searchParams;
+  let pageNumber = page ? parseInt(page) : 1;
 
-    if(isNaN(pageNumber) || pageNumber < 1){
-      pageNumber = 1
-    }
+  if (isNaN(pageNumber) || pageNumber < 1) {
+    pageNumber = 1;
+  }
 
-    const {data, pagination} = await getData(pageNumber)
-    console.log(data)
+  const { data, pagination } = await getData(pageNumber);
+  console.log(data);
 
   return (
     <div className='container mx-auto max-w-3xl space-y-4'>
-        <PageHeader title="Latest Posts"/>
-        <PagePagination pagination={pagination}/>
-        <div className="grid gap-4 justify-items-center">
-          {
-            data.map((post: Post) => (
-              <PageCardImage key={post.id} post={post}/>
-            ))
-          }
-        </div>
-        </div>
-  )
+      <PageHeader title="Latest Posts"/>
+      <PagePagination pagination={pagination}/>
+      <div className="grid gap-4 justify-items-center">
+        {data.map((post: Post) => (
+          <PageCardImage key={post.id} post={post} className="m-4"/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
