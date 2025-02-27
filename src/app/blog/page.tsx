7858d0e34996698258b19676f78577bc41908daf
martinoremonje <m.monjeorellana@gmail.com>
2025-02-27 15:15:22 -0300

    intento 7 de arreglas ahora es el page de blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,30 +5,42 @@ import PageCardImage from '@/components/PageCardImage';
 import { Post } from '@/interfaces/post';
 import PagePagination from '@/components/PagePagination';
 
-const getData = async(page = 1, pageSize = 4) => {
+type Params = Promise<{ slug: string }>;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+const getData = async (page = 1, pageSize = 4) => {
   const path = "/posts";
   const urlParamsObject = {
     populate: "*",
     sort: { createdAt: "asc" },
     pagination: { page, pageSize }
   };
-  
+
   const { data, meta } = await fetchApi(path, urlParamsObject);
   return { data, pagination: meta.pagination };
 }
 
+export async function generateMetadata(props: { 
+  params: Params; 
+  searchParams: SearchParams; 
+}) {
+  const params = await props.params;
+  const searchParams = await props.searchParams;
+  const slug = params.slug;
+  const query = searchParams.query;
+}
+
 interface Props {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    page?: string;
-  };
+  params: Params;
+  searchParams: SearchParams;
 }
 
 const Blog = async ({ params, searchParams }: Props) => {
-  const { page } = searchParams;
-  let pageNumber = page ? parseInt(page) : 1;
+  const paramsResolved = await params;
+  const searchParamsResolved = await searchParams;
+  const { slug } = paramsResolved;
+  const { page } = searchParamsResolved;
+  let pageNumber = page ? parseInt(page as string) : 1;
 
   if (isNaN(pageNumber) || pageNumber < 1) {
     pageNumber = 1;
@@ -39,11 +51,11 @@ const Blog = async ({ params, searchParams }: Props) => {
 
   return (
     <div className='container mx-auto max-w-3xl space-y-4'>
-      <PageHeader title="Latest Posts"/>
-      <PagePagination pagination={pagination}/>
+      <PageHeader title="Latest Posts" />
+      <PagePagination pagination={pagination} />
       <div className="grid gap-4 justify-items-center">
         {data.map((post: Post) => (
-          <PageCardImage key={post.id} post={post}/>
+          <PageCardImage key={post.id} post={post} />
         ))}
       </div>
     </div>
